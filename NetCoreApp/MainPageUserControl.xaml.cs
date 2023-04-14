937ebb4475a6cc97e150f6ac447adf9b228f8265
driver1998 <driver1998.ms@outlook.com>
2023-04-14 10:28:00 +0800

    XAML compiler testing

---
+++ NetCoreApp/MainPageUserControl.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Windows.UI.Xaml.Controls;
+
+namespace NetCoreApp
+{
+    public partial class MainPageUserControl : UserControl
+    {
+        public MainViewModel ViewModel { get; set; }
+    }
+}
+
--- NetCoreApp/MainViewUserControl.cs
+++ NetCoreApp/MainViewUserControl.cs
@@ -5,60 +5,21 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Runtime.InteropServices;
 using Windows.UI.Popups;
-using CSharpMarkup.SystemXaml;
-using static CSharpMarkup.SystemXaml.Helpers;
-using WUXC = Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls;
 using Windows.UI.Xaml.Data;
 using Windows.UI;
 
 namespace NetCoreApp
 {
-    public class MainViewUserControl : WUXC.UserControl
+    public class MainViewUserControl : UserControl
     {
         public MainViewModel ViewModel { get; set; }
 
-        public void BuildUI() =>
-            Content = VStack(
-                TextBox()
-                    .Bind(ViewModel.UserName, BindingMode.TwoWay),
+        public void BuildUI()
+        {
 
-                TextBlock()
-                    .Bind(ViewModel.Message)
-                    .FontSize().Bind(ViewModel.FontSize, fallbackValue: 1.0)
-                    .Foreground().Bind(ViewModel.Color, convert: (Color color) => SolidColorBrush(color)),
-
-                Slider()
-                    .Orientation().Horizontal()
-                    .Minimum(1)
-                    .Maximum(100)
-                    .Bind(ViewModel.FontSize, BindingMode.TwoWay),
-
-                HStack(
-                    Button("Say Hello")
-                        .Invoke((WUXC.Button btn) =>
-                        {
-                            btn.Click += (s, e) => ViewModel.SayHello();
-                        }),
-
-                    Button("About")
-                        .Invoke((WUXC.Button btn) =>
-                        {
-                            btn.Click += async (s, e) =>
-                            {
-                                StringBuilder builder = new();
-                                builder.AppendLine(RuntimeInformation.FrameworkDescription);
-                                builder.AppendLine(RuntimeInformation.OSDescription);
-                                builder.Append($"ProcessArchitecture: {RuntimeInformation.ProcessArchitecture.ToString()}");
-                                MessageDialog dialog = new(builder.ToString(), "About");
-                                await dialog.ShowAsync();
-                            };
-                        })
-                ),
-
-                ColorPicker()
-                    .Color().Bind(ViewModel.Color, BindingMode.TwoWay)
-            );
-
+        }
+            
         public MainViewUserControl()
         {
 
