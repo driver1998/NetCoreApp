060f9d29a5c7609ceb3fe3ba3142bb5a09f93fb8
GH Cao <driver1998.ms@outlook.com>
2024-01-08 14:15:32 +0800

    add AppContainer detection

--- NetCoreApp/MainPageUserControl.xaml.cs
+++ NetCoreApp/MainPageUserControl.xaml.cs
@@ -1,22 +1,64 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
 using System.Runtime.InteropServices;
 using System.Text;
-using System.Threading.Tasks;
 using Windows.UI.Core;
-using Windows.UI.Popups;
 using Windows.UI.Xaml.Controls;
 
 namespace NetCoreApp
 {
+    internal static partial class NativeMethods
+    {
+
+        [LibraryImport("advapi32.dll", SetLastError = true)]
+        private static partial int OpenProcessToken(IntPtr processHandle, uint desiredAccess, out IntPtr tokenHandle);
+
+        [LibraryImport("advapi32.dll", SetLastError = true)]
+        private static partial int GetTokenInformation(IntPtr tokenHandle, int tokenInformationClass, IntPtr tokenInformation, int tokenInformationLength, out int returnLength);
+
+        [LibraryImport("kernel32.dll")]
+        private static partial IntPtr GetCurrentProcess();
+
+        [LibraryImport("kernel32.dll", SetLastError = true)]
+        private static partial int CloseHandle(IntPtr handle);
+
+        private static readonly uint TOKEN_QUERY = 8;
+        private static readonly int TokenIsAppContainer = 29;
+
+        public static bool IsAppContainer()
+        {
+            IntPtr hToken = 0;
+            try
+            {
+                unsafe
+                {
+                    IntPtr hProcess = GetCurrentProcess();
+                    OpenProcessToken(hProcess, TOKEN_QUERY, out hToken);
+
+                    GetTokenInformation(hToken, TokenIsAppContainer, 0, 0, out int infoLength);
+
+                    var info = stackalloc byte[infoLength];
+                    GetTokenInformation(hToken, TokenIsAppContainer, (IntPtr)info, infoLength, out _);
+
+                    bool isAppContainer = *(uint*)info > 0;
+                    return isAppContainer;
+                }
+                
+            }
+            finally
+            {
+                CloseHandle(hToken);
+            }
+        }
+
+    }
+
     public partial class MainPageUserControl : UserControl
     {
         public MainViewModel ViewModel { get; set; }
 
         public MainPageUserControl()
         {
-            this.InitializeComponent();            
+            this.InitializeComponent();
 
             DataContext = ViewModel = new MainViewModel()
             {
@@ -27,12 +69,14 @@ namespace NetCoreApp
             this.btn1.Command = ViewModel.SayHelloCommand;
             this.btn2.Click += async (s, e) =>
             {
-                bool isCoreWindow = CoreWindow.GetForCurrentThread() != null;                
+                bool isCoreWindow = CoreWindow.GetForCurrentThread() != null;
+                bool isAppContainer = NativeMethods.IsAppContainer();
                 StringBuilder builder = new();
                 builder.AppendLine($"IsCoreWindow: {isCoreWindow}");
+                builder.AppendLine($"IsAppContainer: {isAppContainer}");
                 builder.AppendLine($"Runtime: {RuntimeInformation.FrameworkDescription}");
                 builder.AppendLine($"OS: {RuntimeInformation.OSDescription}");
-                builder.Append($"ProcessArchitecture: {RuntimeInformation.ProcessArchitecture.ToString()}");
+                builder.AppendLine($"ProcessArchitecture: {RuntimeInformation.ProcessArchitecture.ToString()}");
                 var contentDialog = new ContentDialog
                 {
                     Title = "About",
@@ -42,8 +86,6 @@ namespace NetCoreApp
                 await contentDialog.ShowAsync();
             };
         }
-
-
     }
 }
 
