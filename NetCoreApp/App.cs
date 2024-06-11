e55d5395b6979f3fdf3a557623788e4fc4ed2c5d
GH Cao <driver1998.ms@outlook.com>
2024-06-11 10:19:22 +0800

    Add WinUI 2

--- NetCoreApp/App.cs
+++ NetCoreApp/App.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using Microsoft.UI.Xaml.Controls;
+using Microsoft.UI.Xaml.XamlTypeInfo;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Runtime.CompilerServices;
@@ -7,21 +9,42 @@ using System.Text;
 using System.Threading.Tasks;
 using Windows.ApplicationModel.Activation;
 using Windows.ApplicationModel.Core;
+using Windows.System;
 using Windows.UI;
 using Windows.UI.ViewManagement;
 using Windows.UI.Xaml;
 using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Markup;
+using Windows.UI.Xaml.Media;
+using MUXC = Microsoft.UI.Xaml.Controls;
 
 namespace NetCoreApp
 {
-    public partial class App : Application
+    public static class UIElementExtensions
     {
+        public static UIElement With<T>(this T self, Action<T> action) where T : UIElement
+        {
+            action(self);
+            return self;
+        }
+    }
+    public partial class App : Application, IXamlMetadataProvider
+    {
+        private readonly List<IXamlMetadataProvider> _providers = new() {
+            new XamlControlsXamlMetaDataProvider()
+        };
+
         protected override void OnWindowCreated(WindowCreatedEventArgs args)
         {
+            this.Resources = new XamlControlsResources();
+
             var window = args.Window;
-            window.Content = new NavigationView
+            var page = new Page();
+            var content = new MUXC.NavigationView
             {
-                PaneDisplayMode = NavigationViewPaneDisplayMode.Left,
+                PaneDisplayMode = MUXC.NavigationViewPaneDisplayMode.Left,
+                Margin = new Thickness(0, 48, 0, 0),
+                IsTitleBarAutoPaddingEnabled = false,
                 Content = new StackPanel
                 {
                     VerticalAlignment = VerticalAlignment.Center,
@@ -39,11 +62,38 @@ namespace NetCoreApp
                         new TextBlock
                         {
                             Text = "IsDynamicCodeCompiled: " + RuntimeFeature.IsDynamicCodeCompiled
+                        },
+                        new TeachingTip
+                        {
+                            Name = "teachingTip",
+                            Title = "WinUI 🎉",
+                            Content = "This is TeachingTip from WinUI 2.",
+                            PreferredPlacement = TeachingTipPlacementMode.Bottom
+                        },
+                        new Button
+                        {                           
+                            Margin = new Thickness(0, 8, 0, 0),
+                            Content = "Show TeachingTip",
                         }
+                        .With(btn =>
+                        {
+                            btn.Click += (sender, e) =>
+                            {
+                                var teachingTip = page.FindName("teachingTip") as TeachingTip;
+                                if (teachingTip != null)
+                                {
+                                    teachingTip.Target = sender as Button;
+                                    teachingTip.IsOpen = true;
+                                }
+                            };
+                        }),
                     }
                 }
             };
-            
+            page.Content = content;
+            window.Content = page;
+            BackdropMaterial.SetApplyToRootOrPageBackground(content, true);
+
             CoreApplication.GetCurrentView().TitleBar.ExtendViewIntoTitleBar = true;
             var titleBar = ApplicationView.GetForCurrentView().TitleBar;
             titleBar.ButtonBackgroundColor = Color.FromArgb(0, 0, 0, 0);
@@ -53,12 +103,43 @@ namespace NetCoreApp
 
         protected override void OnLaunched(LaunchActivatedEventArgs e)
         {
+        }
 
+        public IXamlType? GetXamlType(Type type)
+        {
+            foreach (var provider in _providers)
+            {
+                var xamlType = provider.GetXamlType(type);
+                if (xamlType != null)
+                {
+                    return xamlType;
+                }
+            }
+            return null;
+        }
+
+        public IXamlType? GetXamlType(string fullName)
+        {
+            foreach (var provider in _providers)
+            {
+                var xamlType = provider.GetXamlType(fullName);
+                if (xamlType != null)
+                {
+                    return xamlType;
+                }
+            }
+            return null;
+        }
+
+        public XmlnsDefinition[] GetXmlnsDefinitions()
+        {
+            return _providers.SelectMany(p => p.GetXmlnsDefinitions()).ToArray();            
         }
 
         public static void Main()
         {
             Application.Start(_ => new App());
         }
+
     }
 }
